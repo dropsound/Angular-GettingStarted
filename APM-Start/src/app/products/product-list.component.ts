e6ab916387240c34ac0e4c9325d972c4fce1b3c7
dropsound <dropsoundproduction@gmail.com>
2022-12-23 15:05:10 +0100

    003

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,17 +1,19 @@
-import { Component} from "@angular/core";
+import { Component, OnInit} from "@angular/core";
+import { IProduct } from "./product";
 
 @Component({
     selector: 'apm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     tableTitle: string = "Product List";
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = 'cart';
-    products: any[] = [
+    products: IProduct[] = [
         {
             "productId": 1,
             "productName": "Leaf Rake",
@@ -46,5 +48,9 @@ export class ProductListComponent {
 
     toggleImage(): void {
         this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        console.log('In OnInit');
     }
 }
